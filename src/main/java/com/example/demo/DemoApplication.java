490f3e4311cabf999574253fce0db7307ddf460a
RITA\shaha <shahanaj.pervin0403@gmail.com>
2025-11-20 11:53:47 +0400

    sample stash

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
-        /// sampel_repo...
+        /// sampel_repo...1234
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
