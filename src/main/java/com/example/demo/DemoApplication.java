76989db7944c9e15675fb2e937af07e67ced4442
RITA\shaha <shahanaj.pervin0403@gmail.com>
2025-11-20 11:45:26 +0400

    sample git test 2

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
-        /// sampel_repo...123
+        /// sampel_repo...1232
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
